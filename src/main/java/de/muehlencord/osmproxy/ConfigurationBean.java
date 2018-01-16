42829d12974408c26e25d784c3616578c1b5ec65
jomu <joern@muehlencord.de>
2018-01-16 09:39:54 +0100

    added license header

--- src/main/java/de/muehlencord/osmproxy/ConfigurationBean.java
+++ src/main/java/de/muehlencord/osmproxy/ConfigurationBean.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Joern Muehlencord <joern at muehlencord.de>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.muehlencord.osmproxy;
 
 import de.muehlencord.osmproxy.business.config.entity.ConfigurationException;
@@ -21,10 +36,24 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class ConfigurationBean {
 
+    /**
+     * the configuration to be used
+     */
     private Configuration configuration;
 
+    /**
+     * the logger
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationBean.class);
 
+    /**
+     * return the configuration object. If not yet loaded, it is loaded from
+     * disk.
+     *
+     * @return the configuration object.
+     * @throws ConfigurationException if the configuration cannot be loaded from
+     * disk.
+     */
     protected Configuration getConfiguration() throws ConfigurationException {
         if (configuration == null) {
             URL url = ConfigurationBean.class.getResource("osmproxy.cfg"); // FIXME needs to be tested, will be null at the moment
@@ -57,6 +86,15 @@ public class ConfigurationBean {
         return configuration;
     }
 
+    /**
+     * returns the cache directory for the given layer
+     *
+     * @param layerName the name of the layer to return the cache for - e.g.
+     * tiles.
+     * @return the directory where to store the tiles to.
+     * @throws ConfigurationException if the configuration is not loaded or the
+     * cache directory is not defined.
+     */
     public Path getCacheDirectory(String layerName) throws ConfigurationException {
         Cache cache = getConfiguration().getCache();
         if (cache == null) {
@@ -75,6 +113,15 @@ public class ConfigurationBean {
         return path;
     }
 
+    /**
+     * returns a list of servers to use as upstream servers.
+     *
+     * @param layerName the name of the layer to get the upstream server for -
+     * e.g. tiles.
+     * @return a list of servers to use as upstream servers.
+     * @throws ConfigurationException if the configuration is not loaded or does
+     * not contain any upstream server definition.
+     */
     public List<Server> getUpstreamServer(String layerName) throws ConfigurationException {
         Layer layer = getConfiguration().getLayer(layerName);
         if (layer == null) {
